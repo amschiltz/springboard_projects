924a7ac9b11124071cca8d5cd39739ed70d02650
amschiltz <allisonschiltz@gmail.com>
2025-09-20 18:44:46 -0600

    Update SQLTasks Tier 1-checkpoint_AMS.sql

--- Case_Study-Country_Club/SQLFiles_Tier_1/SQLTasks Tier 1-checkpoint_AMS.sql
+++ Case_Study-Country_Club/SQLFiles_Tier_1/SQLTasks Tier 1-checkpoint_AMS.sql
@@ -33,36 +33,77 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
+    
+SELECT 
+    name, 
+    membercost 
+FROM Facilities 
+WHERE membercost > 0
 
 
 /* Q2: How many facilities do not charge a fee to members? */
-
+    
+SELECT 
+    name, 
+    membercost 
+FROM Facilities
+WHERE membercost = 0
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-
+    
+SELECT 
+    facid, 
+    name, 
+    membercost, 
+    monthlymaintenance 
+FROM Facilities 
+WHERE membercost > 0 
+    AND (membercost / monthlymaintenance < .2)
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
+SELECT * 
+FROM Facilities 
+WHERE facid IN (1, 5)
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name,
+        monthlymaintenance,
+        CASE WHEN monthlymaintenance > 100 THEN 'Expensive'
+            ELSE 'cheap' END AS cost_status
+FROM Facilities
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+SELECT
+    firstname,
+    surname,
+    joindate
+FROM Members
+WHERE joindate = (SELECT MAX(joindate) FROM Members)
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
+SELECT DISTINCT
+    Members.firstname || ' ' || Members.surname AS member_name,
+    Facilities.name AS facility_name
+FROM Members
+INNER JOIN Bookings USING(memid)
+INNER JOIN Facilities USING(facid)
+WHERE Facilities.name LIKE 'Tennis Court%'
+ORDER BY member_name, facility_name
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -71,9 +112,50 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+SELECT
+    Facilities.name AS facility_name,
+    Members.firstname || ' ' || Members.surname AS member_name,
+    CASE 
+        WHEN Bookings.memid = 0
+        THEN Facilities.guestcost * Bookings.slots
+        ELSE Facilities.membercost * Bookings.slots
+    END AS cost
+FROM Members
+INNER JOIN Bookings USING(memid)
+INNER JOIN Facilities USING(facid)
+WHERE starttime >= '2012-09-14 00:00:00' 
+    AND starttime < '2012-09-15 00:00:00'
+    AND (
+    CASE 
+        WHEN Bookings.memid = 0
+        THEN Facilities.guestcost * Bookings.slots
+        ELSE Facilities.membercost * Bookings.slots 
+    END
+    ) > 30
+ORDER BY cost DESC
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+SELECT 
+facility_name, 
+member_name, 
+cost
+FROM (
+    SELECT
+    Facilities.name AS facility_name,
+    Members.firstname || ' ' || Members.surname AS member_name,
+    CASE
+        WHEN Bookings.memid = 0 THEN Facilities.guestcost * Bookings.slots
+        ELSE Facilities.membercost * Bookings.slots
+    END AS cost
+    FROM Bookings
+    JOIN Members ON Bookings.memid = Members.memid
+    JOIN Facilities ON Bookings.facid = Facilities.facid
+    WHERE Bookings.starttime >= '2012-09-14 00:00:00'
+    AND Bookings.starttime <  '2012-09-15 00:00:00'
+) AS priced
+WHERE cost > 30
+ORDER BY cost DESC
 
 /* PART 2: SQLite
 /* We now want you to jump over to a local instance of the database on your machine. 
@@ -95,11 +177,58 @@ QUESTIONS:
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
 
+SELECT 
+Facilities.name AS facility_name, 
+SUM(
+    CASE
+    WHEN Bookings.memid = 0 THEN Facilities.guestcost * Bookings.slots
+    ELSE Facilities.membercost * Bookings.slots
+    END
+) AS total_revenue
+FROM Bookings
+JOIN Members ON Bookings.memid = Members.memid
+JOIN Facilities ON Bookings.facid = Facilities.facid
+GROUP BY facility_name
+HAVING total_revenue < 1000
+ORDER BY total_revenue DESC
+
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
 
+SELECT 
+    m.memid,
+    m.surname,
+    m.firstname,
+    r.surname AS recommender_surname,
+    r.firstname AS recommender_firstname
+FROM Members m
+LEFT JOIN Members r 
+    ON CAST(m.recommendedby AS INTEGER) = r.memid
+WHERE m.recommendedby IS NOT NULL 
+    AND m.recommendedby != ''
+ORDER BY m.surname, m.firstname
 
 /* Q12: Find the facilities with their usage by member, but not guests */
 
+SELECT 
+    Facilities.name AS facility_name,
+    Members.firstname || ' ' || Members.surname AS member_name,
+    COUNT(slots) AS slots_count
+FROM Bookings
+JOIN Facilities USING(facid)
+JOIN Members USING(memid)
+WHERE memid != 0
+GROUP BY Facilities.name, memid
+ORDER BY Facilities.name, member_name
 
 /* Q13: Find the facilities usage by month, but not guests */
 
+SELECT 
+    Facilities.name AS facility_name,
+    strftime('%m', starttime) AS month,
+    COUNT(slots) AS slots_count
+FROM Bookings
+JOIN Facilities USING(facid)
+JOIN Members USING(memid)
+WHERE memid != 0
+GROUP BY Facilities.name, month
+ORDER BY Facilities.name, month
